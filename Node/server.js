97fe49a4f450cc8aaeb215fb381d52ac7417e939
brennanho <brennanmho@gmail.com>
2019-01-16 02:08:23 -0800

    Going to sleep...

--- Node/server.js
+++ Node/server.js
@@ -10,14 +10,13 @@ let ids = []; //For priority queue
 let sockets = {}; //Maintain player sockets
 
 //'Assign random public DNS'
-const tunnel = localtunnel(port, {subdomain: 'testing12345'}, (err, tunnel) => {
+const tunnel = localtunnel(port, {subdomain: 'testing123456'}, (err, tunnel) => {
     console.log('Connect to', tunnel.url);
 });
 
 wss.on('connection', function connection(ws) {
 	
 	//Player is searching for an opponent
-	ws.send("ID" + id.toString());
 	ids.unshift(id);
 	sockets[id++] = ws;
 	console.log("Players in queue:", Object.keys(sockets).length);
@@ -27,11 +26,18 @@ wss.on('connection', function connection(ws) {
 		msg = "" + msg;
 		if (msg == "Find Match") {
 			while (ids.length >= 2) {
-				let player_1_ID = ids.pop();
-				let player_2_ID = ids.pop();
-				sockets[player_1_ID].send('Opponent ID' + player_2_ID.toString());
-				sockets[player_2_ID].send('Opponent ID' + player_1_ID.toString());
-				console.log(player_1_ID.toString(), "V.S", player_2_ID.toString()); 
+				try {
+					let player_1_ID = ids.pop();
+					let player_2_ID = ids.pop();
+					console.log(Object.keys(sockets), player_1_ID, player_2_ID);
+					sockets[player_1_ID].send('ID' + player_1_ID.toString() + ', OppID' + player_2_ID.toString());
+					sockets[player_2_ID].send('ID' + player_2_ID.toString() + ', OppID' + player_1_ID.toString());
+					delete sockets[player_1_ID];
+					delete sockets[player_2_ID];
+					console.log(player_1_ID.toString(), "V.S", player_2_ID.toString()); 
+				} catch(err) {
+					console.log(err);
+				}
 			}
 		}
 	});
@@ -40,15 +46,16 @@ wss.on('connection', function connection(ws) {
 	ws.on('close', function(data, flags) {
 		//Inefficient way to remove disconnected player from queue
 		for (let id in sockets) {
-			if (sockets[id] == ws) {
+			if (sockets[id] === ws) {
 				console.log("Player ID:", id, "has disconnected");
 				delete sockets[id];
-				for( let i = 0; i < ids.length-1; i++){ 
-   					if ( ids[i] == id) {
+				for( let i = 0; i < ids.length; i++){ 
+   					if (ids[i] == id) {
      					ids.splice(i, 1);
-     					return; 
+     					break;
   					}
 				}
+				break;
 			}
 		}
 
--- scripts/node_client.gd
+++ scripts/node_client.gd
@@ -3,40 +3,39 @@ var ws = WebSocketClient.new()
 var wss = WebSocketServer.new()
 var wsp = WebSocketPeer.new()
 var socket_id 
-var opponent_socket_id
-const server_url = "ws://testing12345.localtunnel.me/socket.io/?EIO=3&transport=websocket"
+var opp_socket_id
+const server_url = "ws://testing123456.localtunnel.me/socket.io/?EIO=3&transport=websocket"
 const localhost = "ws://127.0.0.1:8092/socket.io/?EIO=3&transport=websocket"
 	
 #Websocket signals 
 func _connection_established(protocol):
-	print("Connection established with server")
+	var msg = "Find Match".to_utf8()
+	print(ws.get_peer(1).put_packet(msg))
 func _connection_closed(reason):
 	print("Connection closed:", reason)
 	ws.get_peer(1).put_packet("ID: " + socket_id + "has disconnected")
-	
 func _connection_error():
-	print("Connection error")
+	print("Could not connect to server")
 	
 func _data_received():
 	var msg = ws.get_peer(1).get_packet().get_string_from_utf8()
 	print(msg)
 	if msg.begins_with("ID"):
-		socket_id = msg.substr(2, len(msg))
-		print("My socket ID: " + socket_id)
-	elif msg.begins_with("Opponent ID"):
-		opponent_socket_id = msg.substr(11, len(msg))
-		print("My opponent ID: " + opponent_socket_id)
+		msg = msg.split(",")
+		socket_id = msg[0].substr(2,len(msg[0]))
+		opp_socket_id = msg[1].substr(6, len(msg[1]))
+		print("My ID: ", socket_id)
+		print("Opp ID: ", opp_socket_id)
 	
 func _process(delta):
 	if ws.CONNECTION_CONNECTED:
 		ws.poll()
 		
 func _on_Find_Opponent_button_down():
-	if ws.CONNECTION_CONNECTED:
-		ws.connect_to_url(server_url, PoolStringArray(['hello']), false)
-		var msg = "Find Match".to_utf8()
-		ws.get_peer(1).put_packet(msg)
-		self.disabled = true
+	set_process(true)
+	while ws.connect_to_url(server_url, PoolStringArray(['hello']), false) != 0:
+		print("Attempting to connect to server...")
+	self.disabled = true
 	
 #Initialize websocket signals
 func _ready():
@@ -44,4 +43,4 @@ func _ready():
 	ws.connect("connection_closed", self, "_connection_closed")
 	ws.connect("connection_error", self, "_connection_error")
 	ws.connect("data_received", self, "_data_received")
-	set_process(true)
+	set_process(false)
