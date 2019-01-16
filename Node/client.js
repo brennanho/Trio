e0c97d7b86c8913b5b66537e487dd01b69ddb8d9
brennanho <brennanmho@gmail.com>
2019-01-15 22:53:40 -0800

    Server skeleton working

--- Node/client.js
+++ Node/client.js
@@ -1,1 +1,10 @@
-const express = require('express');
+const io = require('socket.io-client');
+const socket = io('https://testing12345.localtunnel.me');
+
+socket.on('message', function(msg) {
+    console.log(msg);
+})
+
+socket.emit('message', 'Hello from client');
+
+socket.close()
--- Node/server.js
+++ Node/server.js
@@ -1,30 +1,18 @@
-// Server for match making system
-
-const http = require('http');
+//Server for match making system
+const WebSocketServer = require('ws').Server;
 const localtunnel = require('localtunnel');
-const express = require('express');
-const socket = require('socket.io');
-
 const port = 8092;
-const server = http.createServer(express());
+const wss = new WebSocketServer({ port: port });
 
-// 'Deploy webapp'
-const tunnel = localtunnel(port, { subdomain: 'sdfdsfqgqgqerqezdczxcfeqfzxcv'}, (err, tunnel) => {
+//'Assign random public DNS'
+const tunnel = localtunnel(port, {subdomain: 'testing12345'}, (err, tunnel) => {
     console.log(tunnel.url);
 });
 
-// Initiate server
-server.listen(port, () => console.log('Listening on port', port));
-
-let io = socket(server);
-let players = [];
-io.on('connection', function(socket) {
-    console.log('Client connected:', socket.id);
-    players.unshift(socket.id);
-
- 	console.log(players);
-})
-
-io.on('message', function(msg) {
-	console.log(msg);
-})
+wss.on('connection', function connection(ws) {
+	ws.send('Greetings from server');
+	ws.on('message', function message(msg) {
+		msg = "" + msg;
+		console.log(""+ msg);
+	});
+});
