36a3be16f2b6e1b7c58a140e7b7c667b4e2a7c8e
brennanho <brennanmho@gmail.com>
2019-01-16 00:37:25 -0800

    Testing find game

--- Node/server.js
+++ Node/server.js
@@ -1,18 +1,56 @@
 //Server for match making system
+
 const WebSocketServer = require('ws').Server;
 const localtunnel = require('localtunnel');
 const port = 8092;
 const wss = new WebSocketServer({ port: port });
 
+let id = 0;
+let ids = []; //For priority queue
+let sockets = {}; //Maintain player sockets
+
 //'Assign random public DNS'
 const tunnel = localtunnel(port, {subdomain: 'testing12345'}, (err, tunnel) => {
-    console.log(tunnel.url);
+    console.log('Connect to', tunnel.url);
 });
 
 wss.on('connection', function connection(ws) {
-	ws.send('Greetings from server');
+	
+	//Player is searching for an opponent
+	ws.send("ID" + id.toString());
+	ids.unshift(id);
+	sockets[id++] = ws;
+	console.log("Players in queue:", Object.keys(sockets).length);
+
+	//Player send message to server
 	ws.on('message', function message(msg) {
 		msg = "" + msg;
-		console.log(""+ msg);
+		if (msg == "Find Match") {
+			while (ids.length >= 2) {
+				let player_1_ID = ids.pop();
+				let player_2_ID = ids.pop();
+				sockets[player_1_ID].send('Opponent ID: ' + player_2_ID.toString());
+				sockets[player_2_ID].send('Opponent ID: ' + player_1_ID.toString()); 
+			}
+		}
 	});
+
+	//Player disconnected from server
+	ws.on('close', function(data, flags) {
+		//Inefficient way to remove disconnected player from queue
+		for (let id in sockets) {
+			if (sockets[id] == ws) {
+				console.log("Player ID:", id, "has disconnected");
+				delete sockets[id];
+				for( let i = 0; i < ids.length-1; i++){ 
+   					if ( ids[i] == id) {
+     					arr.splice(i, 1);
+     					return; 
+  					}
+				}
+			}
+		}
+
+	});
+
 });
--- scripts/node_client.gd
+++ scripts/node_client.gd
@@ -2,19 +2,25 @@ extends Button
 var ws = WebSocketClient.new()
 var wss = WebSocketServer.new()
 var wsp = WebSocketPeer.new()
+var socket_id 
 const server_url = "ws://testing12345.localtunnel.me/socket.io/?EIO=3&transport=websocket"
-const local_url = "ws://127.0.0.1:8092/socket.io/?EIO=3&transport=websocket"
+const localhost = "ws://127.0.0.1:8092/socket.io/?EIO=3&transport=websocket"
 	
 #Websocket signals 
 func _connection_established(protocol):
-	print("Connection established with protocol: ", protocol)
+	print("Connection established with server")
 func _connection_closed(reason):
 	print("Connection closed:", reason)
+	ws.get_peer(1).put_packet("ID: " + socket_id + "has disconnected")
+	
 func _connection_error():
 	print("Connection error")
+	
 func _data_received():
 	var msg = ws.get_peer(1).get_packet().get_string_from_utf8()
-	print(msg)
+	if msg.begins_with("ID"):
+		socket_id = msg.substr(2, len(msg))
+		print("My socket ID: " + socket_id)
 	
 func _process(delta):
 	if ws.CONNECTION_CONNECTED:
@@ -22,8 +28,9 @@ func _process(delta):
 		
 func _on_Find_Opponent_button_down():
 	if ws.CONNECTION_CONNECTED:
-		var msg = "Hello from Godot".to_utf8()
+		var msg = "Find Match".to_utf8()
 		ws.get_peer(1).put_packet(msg)
+		self.disabled = true
 	
 #Initialize websocket signals
 func _ready():
@@ -31,5 +38,5 @@ func _ready():
 	ws.connect("connection_closed", self, "_connection_closed")
 	ws.connect("connection_error", self, "_connection_error")
 	ws.connect("data_received", self, "_data_received")
-	ws.connect_to_url(server_url, PoolStringArray([]), false)
+	ws.connect_to_url(server_url, PoolStringArray(['hello']), false)
 	set_process(true)
