5b31b69eb716fcfc2ad827c9f301eef31d0f639d
brennanho <brennanmho@gmail.com>
2019-01-20 01:19:48 -0800

    Server code cleanup

--- Node/server.js
+++ Node/server.js
@@ -7,8 +7,7 @@ const wss = new WebSocketServer({ port: port });
 
 let id = 0;					
 let ids = []; //For priority queue
-let sockets = {}; //Maintain player sockets
-let opponent_ids = {}; //Maintain 
+let players = {}; //Dictionary of players that retain socket IDS and socket objects
 
 console.log("Starting server on port:", port);
 
@@ -21,22 +20,21 @@ wss.on('connection', function connection(ws) {
 
 	//Player is searching for an opponent
 	ids.unshift(id);
-	sockets[id++] = ws;
+	players[id] = {socket: ws, my_ID: id++};
 	console.log("Players in queue:", ids.length);
 
 	//Player send message to server
 	ws.on('message', function message(msg) {
 		msg = "" + msg;
-		//console.log(msg);
 		if (msg == "Find Match") {
 			while (ids.length >= 2) {
 				let player_1_ID = ids.pop();
 				let player_2_ID = ids.pop();
-				opponent_ids[player_1_ID] = player_2_ID;
-				opponent_ids[player_2_ID] = player_1_ID;
+				players[player_1_ID].opponent_ID = player_2_ID;
+				players[player_2_ID].opponent_ID = player_1_ID;
 				let seed = Math.floor(Math.random() * 1000).toString();
-				sockets[player_1_ID].send('ID' + player_1_ID.toString() + ', OppID' + player_2_ID.toString() + ',' + seed);
-				sockets[player_2_ID].send('ID' + player_2_ID.toString() + ', OppID' + player_1_ID.toString() + ',' + seed);
+				players[player_2_ID].socket.send('ID' + player_2_ID.toString() + ', OppID' + player_1_ID.toString() + ',' + seed);
+				players[player_1_ID].socket.send('ID' + player_1_ID.toString() + ', OppID' + player_2_ID.toString() + ',' + seed);
 				console.log(player_1_ID.toString(), "V.S", player_2_ID.toString()); 
 			}
 		}  // In game messaging i.e. opponent found a set
@@ -44,7 +42,7 @@ wss.on('connection', function connection(ws) {
 			msg = msg.split(",")
 			let opp_id = parseInt(msg[1]);
 			let set = msg[2] + "," + msg[3] + "," + msg[4];
-			sockets[opp_id].send("SET," + set);
+			players[opp_id].socket.send("SET," + set);
 			console.log("Set sent to Opp_ID:", opp_id.toString());
 		} 
 		else if (msg == "PING") {
@@ -58,19 +56,13 @@ wss.on('connection', function connection(ws) {
 	//Player disconnected from server
 	ws.on('close', function(data, flags) {
 		//Inefficient way to remove disconnected player from queue
-		for (let id in sockets) {
-			if (sockets[id] === ws) {
-				console.log("Player ID:", id, "has disconnected");
-				try {
-					sockets[opponent_ids[id]].send('WIN');
-					delete opponent_ids[id];
-					delete opponent_ids[opponent_ids[id]];
-					delete sockets[id];
-					delete sockets[opponent_ids[id]];
-	     			ids.splice(ids.indexOf(id), 1);
-	     		} catch (err) {
-	     			//
-	     		}
+		for (const [id, player] of Object.entries(players)) {
+			if (player.socket == ws) {
+				console.log("Player ID:", player.opponent_ID, "has disconnected");
+				players[player.opponent_ID].socket.send('WIN');
+				delete players[id];
+				delete players[player.opponent_ID];
+     			ids.splice(ids.indexOf(id), 1);
      			break;
 			}
 		}
